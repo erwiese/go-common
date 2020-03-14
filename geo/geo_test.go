06407f40ac13074677256c345b2b7a32d725850f
wiese <erwinwiesensarter@web.de>
2020-03-14 22:44:54 +0100

    ADD: geo conversion

--- geo/geo_test.go
+++ geo/geo_test.go
@@ -36,3 +36,36 @@ func TestXYZ2ell(t *testing.T) {
 		})
 	}
 }
+
+func TestEll2XYZ(t *testing.T) {
+	type ell struct {
+		n  float64
+		e  float64
+		up float64
+	}
+	tests := []struct {
+		name  string
+		ell   ell
+		wantX float64
+		wantY float64
+		wantZ float64
+	}{
+
+		{name: "LEIJ", ell: ell{51.3539791666985, 12.374095866237646, 178.3889}, wantX: 3898736.515, wantY: 855345.125, wantZ: 4958372.370},
+		{name: "WTZR", ell: ell{49.144199946431826, 12.878912704434896, 666.0298}, wantX: 4075580.384, wantY: 931853.979, wantZ: 4801568.245},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotX, gotY, gotZ := Ell2XYZ(tt.ell.n, tt.ell.e, tt.ell.up)
+			if math.Round(gotX*1000)/1000 != tt.wantX {
+				t.Errorf("Ell2XYZ() gotX = %v, want %v", gotX, tt.wantX)
+			}
+			if math.Round(gotY*1000)/1000 != tt.wantY {
+				t.Errorf("Ell2XYZ() gotY = %v, want %v", gotY, tt.wantY)
+			}
+			if math.Round(gotZ*1000)/1000 != tt.wantZ {
+				t.Errorf("Ell2XYZ() gotZ = %v, want %v", gotZ, tt.wantZ)
+			}
+		})
+	}
+}
